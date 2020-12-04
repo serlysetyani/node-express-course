3e2817f8d454c2cc4a0f5cec53c7a3941bacdd52
serlyset <serlysetyani@student.telkomuniversity.ac.id>
2020-12-04 14:01:12 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData =[
     {name : 'Serly'},
@@ -20,6 +23,27 @@ app.get('/users/:id', function(req,res){
         message: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/login', function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "serlyset";
+    const mockPassword = "123"
+
+    if (username === mockUsername && password === mockPassword){
+        res.json({
+            success: true,
+            message: 'username and password match',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'username and password do not match'
+        })
+    }
 })
 
 app.listen(8000, function(){
